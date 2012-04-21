e0861d5bc4d2835c98f2d258460bab5e48948b41
Sindre Sorhus <sindresorhus@gmail.com>
2012-04-22 01:56:38 +0200

    Code style and spacing tweaks

--- src/screenfull.js
+++ src/screenfull.js
@@ -27,9 +27,12 @@
 					'mozFullScreen',
 					'mozFullScreenElement'
 				]
-			];
-			for ( var i = 0, l = methodMap.length; i < l; i++ ) {
-				var val = methodMap[i];
+			],
+			i = 0,
+			l = methodMap.length;
+
+			for ( ; i < l; i++ ) {
+				var val = methodMap[ i ];
 				if ( val[1] in document ) {
 					return val;
 				}
@@ -38,26 +41,34 @@
 
 		screenfull = {
 			isFullscreen: document[ methods[3] ],
+
 			element: document[ methods[4] ],
+
 			request: function( elem ) {
 				var request = methods[0];
+
 				elem = elem || document.documentElement;
-				// If you request a new element when already in fullscreen, Chrome
-				// will change directly to that element, while Firefox will do nothing.
-				// Force Firefox to change element by exiting and then reenter, making it consistent.
+
+				// If you request a new element when already in fullscreen, Chrome will
+				// change directly to that element, while Firefox will do nothing. Force
+				// Firefox to change element by exiting and then reenter, making it consistent.
 				if ( request.indexOf('moz') !== -1 && elem !== this.element ) {
 					this.exit();
 				}
+
 				elem[ request ]( keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT );
+
 				// Work around Safari 5.1 bug: reports support for
 				// keyboard in fullscreen even though it doesn't.
 				if ( !document.isFullscreen ) {
 					elem[ request ]();
 				}
 			},
+
 			exit: function() {
 				document[ methods[1] ]();
 			},
+
 			toggle: function( elem ) {
 				if ( this.isFullscreen ) {
 					this.exit();
@@ -65,11 +76,12 @@
 					this.request( elem );
 				}
 			},
+
 			onchange: function() {}
 		};
 
 	if ( !methods ) {
-		return window.screenfull = false;
+		return;
 	}
 
 	document.addEventListener( methods[2], function( e ) {
