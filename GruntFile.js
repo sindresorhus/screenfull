928c2c9a6fb83dcd958b375830505fc10921ba8f
Paul Grock <paulgrock@gmail.com>
2013-09-06 20:15:32 +0200

    Support for IE11 and Grunt 0.4 Fixes #15 and #21

---
+++ GruntFile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+	'use strict';
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		meta: {
+			banner: '/*!\n' +
+				'* <%= pkg.name %>\n' +
+				'* v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+				'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+				'* (c) <%= pkg.author.name %>;' +
+				' <%= _.pluck(pkg.licenses, "type").join(", ") %> License\n' +
+				'*/\n'
+		},
+		concat: {
+			options: {
+				banner: '<%= meta.banner %>'
+			},
+			dist: {
+				src: [
+					'src/screenfull.js'
+				],
+				dest: 'dist/screenfull.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= meta.banner %>'
+			},
+			dist: {
+				files: {
+					'dist/screenfull.min.js':	['src/screenfull.js']
+				}
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: ['GruntFile.js', 'src/screenfull.js']
+		}
+	});
+
+	grunt.registerTask('default', 'jshint');
+	grunt.registerTask('release', [
+		'jshint',
+		'concat',
+		'uglify'
+	]);
+};
