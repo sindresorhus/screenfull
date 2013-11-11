6daf646545c3ccf991f2edddedd90a7026eb3e5b
Andy Dayton <amdayton@gmail.com>
2013-11-11 09:47:18 -0500

    Added AMD compatibility

--- src/screenfull.js
+++ src/screenfull.js
@@ -1,137 +1,149 @@
 /*global Element */
-(function (window, document) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else {
+        root.screenfull = factory();
+    }
+}(this, function () {
+
 	'use strict';
 
-	var keyboardAllowed = typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element, // IE6 throws without typeof check
+	return (function (window, document) {
 
-		fn = (function () {
-			var val, valLength;
-			var fnMap = [
-				[
-					'requestFullscreen',
-					'exitFullscreen',
-					'fullscreenElement',
-					'fullscreenEnabled',
-					'fullscreenchange',
-					'fullscreenerror'
-				],
-				// new WebKit
-				[
-					'webkitRequestFullscreen',
-					'webkitExitFullscreen',
-					'webkitFullscreenElement',
-					'webkitFullscreenEnabled',
-					'webkitfullscreenchange',
-					'webkitfullscreenerror'
+		var keyboardAllowed = typeof Element !== 'undefined' && 'ALLOW_KEYBOARD_INPUT' in Element, // IE6 throws without typeof check
 
-				],
-				// old WebKit (Safari 5.1)
-				[
-					'webkitRequestFullScreen',
-					'webkitCancelFullScreen',
-					'webkitCurrentFullScreenElement',
-					'webkitCancelFullScreen',
-					'webkitfullscreenchange',
-					'webkitfullscreenerror'
+			fn = (function () {
+				var val, valLength;
+				var fnMap = [
+					[
+						'requestFullscreen',
+						'exitFullscreen',
+						'fullscreenElement',
+						'fullscreenEnabled',
+						'fullscreenchange',
+						'fullscreenerror'
+					],
+					// new WebKit
+					[
+						'webkitRequestFullscreen',
+						'webkitExitFullscreen',
+						'webkitFullscreenElement',
+						'webkitFullscreenEnabled',
+						'webkitfullscreenchange',
+						'webkitfullscreenerror'
 
-				],
-				[
-					'mozRequestFullScreen',
-					'mozCancelFullScreen',
-					'mozFullScreenElement',
-					'mozFullScreenEnabled',
-					'mozfullscreenchange',
-					'mozfullscreenerror'
-				],
-				[
-					'msRequestFullscreen',
-					'msExitFullscreen',
-					'msFullscreenElement',
-					'msFullscreenEnabled',
-					'MSFullscreenchange',
-					'MSFullscreenerror'
-				]
-			];
-			var i = 0;
-			var l = fnMap.length;
-			var ret = {};
+					],
+					// old WebKit (Safari 5.1)
+					[
+						'webkitRequestFullScreen',
+						'webkitCancelFullScreen',
+						'webkitCurrentFullScreenElement',
+						'webkitCancelFullScreen',
+						'webkitfullscreenchange',
+						'webkitfullscreenerror'
 
-			for (; i < l; i++) {
-				val = fnMap[i];
-				if (val && val[1] in document) {
-					for (i = 0, valLength = val.length; i < valLength; i++) {
-						ret[fnMap[0][i]] = val[i];
+					],
+					[
+						'mozRequestFullScreen',
+						'mozCancelFullScreen',
+						'mozFullScreenElement',
+						'mozFullScreenEnabled',
+						'mozfullscreenchange',
+						'mozfullscreenerror'
+					],
+					[
+						'msRequestFullscreen',
+						'msExitFullscreen',
+						'msFullscreenElement',
+						'msFullscreenEnabled',
+						'MSFullscreenchange',
+						'MSFullscreenerror'
+					]
+				];
+				var i = 0;
+				var l = fnMap.length;
+				var ret = {};
+
+				for (; i < l; i++) {
+					val = fnMap[i];
+					if (val && val[1] in document) {
+						for (i = 0, valLength = val.length; i < valLength; i++) {
+							ret[fnMap[0][i]] = val[i];
+						}
+						return ret;
 					}
-					return ret;
 				}
-			}
-			return false;
-		})(),
+				return false;
+			})(),
 
-		screenfull = {
-			request: function (elem) {
-				var request = fn.requestFullscreen;
+			screenfull = {
+				request: function (elem) {
+					var request = fn.requestFullscreen;
 
-				elem = elem || document.documentElement;
+					elem = elem || document.documentElement;
 
-				// Work around Safari 5.1 bug: reports support for
-				// keyboard in fullscreen even though it doesn't.
-				// Browser sniffing, since the alternative with
-				// setTimeout is even worse.
-				if (/5\.1[\.\d]* Safari/.test(navigator.userAgent)) {
-					elem[request]();
-				} else {
-					elem[request](keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT);
-				}
-			},
-			exit: function () {
-				document[fn.exitFullscreen]();
-			},
-			toggle: function (elem) {
-				if (this.isFullscreen) {
-					this.exit();
-				} else {
-					this.request(elem);
-				}
-			},
-			onchange: function () {},
-			onerror: function () {},
-			raw: fn
-		};
+					// Work around Safari 5.1 bug: reports support for
+					// keyboard in fullscreen even though it doesn't.
+					// Browser sniffing, since the alternative with
+					// setTimeout is even worse.
+					if (/5\.1[\.\d]* Safari/.test(navigator.userAgent)) {
+						elem[request]();
+					} else {
+						elem[request](keyboardAllowed && Element.ALLOW_KEYBOARD_INPUT);
+					}
+				},
+				exit: function () {
+					document[fn.exitFullscreen]();
+				},
+				toggle: function (elem) {
+					if (this.isFullscreen) {
+						this.exit();
+					} else {
+						this.request(elem);
+					}
+				},
+				onchange: function () {},
+				onerror: function () {},
+				raw: fn
+			};
 
-	if (!fn) {
-		window.screenfull = false;
-		return;
-	}
-
-	Object.defineProperties(screenfull, {
-		isFullscreen: {
-			get: function () {
-				return !!document[fn.fullscreenElement];
-			}
-		},
-		element: {
-			enumerable: true,
-			get: function () {
-				return document[fn.fullscreenElement];
-			}
-		},
-		enabled: {
-			enumerable: true,
-			get: function () {
-				// Coerce to boolean in case of old WebKit
-				return !!document[fn.fullscreenEnabled];
-			}
+		if (!fn) {
+			window.screenfull = false;
+			return;
 		}
-	});
 
-	document.addEventListener(fn.fullscreenchange, function (e) {
-		screenfull.onchange.call(screenfull, e);
-	});
+		Object.defineProperties(screenfull, {
+			isFullscreen: {
+				get: function () {
+					return !!document[fn.fullscreenElement];
+				}
+			},
+			element: {
+				enumerable: true,
+				get: function () {
+					return document[fn.fullscreenElement];
+				}
+			},
+			enabled: {
+				enumerable: true,
+				get: function () {
+					// Coerce to boolean in case of old WebKit
+					return !!document[fn.fullscreenEnabled];
+				}
+			}
+		});
 
-	document.addEventListener(fn.fullscreenerror, function (e) {
-		screenfull.onerror.call(screenfull, e);
-	});
+		document.addEventListener(fn.fullscreenchange, function (e) {
+			screenfull.onchange.call(screenfull, e);
+		});
 
-	window.screenfull = screenfull;
-})(window, document);
+		document.addEventListener(fn.fullscreenerror, function (e) {
+			screenfull.onerror.call(screenfull, e);
+		});
+
+	    //value to use for the public API for the built file.
+	    return screenfull;
+
+	})(window, document);
+}));
